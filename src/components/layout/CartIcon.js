bf1f60c37f800587e89e70267a87211feec22a60
Sagi Korzack <37194830+sagiK11@users.noreply.github.com>
2020-09-19 19:13:01 +0300

    Navbar Cart Icon

--- src/components/layout/CartIcon.js
+++ src/components/layout/CartIcon.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Badge from '@material-ui/core/Badge';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
-import Grid from '@material-ui/core/Grid';
-import Icon from '@material-ui/core/Icon';
 
 const CartIcon = (props) => {
     return (
         <NavLink to="/cart" className="nav-cart">
-            <Grid container >
-                <span className="cart-length fa pink-text lighten-5 "
-                    style={{ marginTop: "0.6em" }}>{props.itemsNumber}</span>
-                <span><Icon>shopping_basket_icon</Icon></span>
-            </Grid>
+            <Badge badgeContent={props.itemsNumber} showZero color="primary">
+                <ShoppingBasketIcon style={{ color: "black" }} />
+            </Badge>
         </NavLink>
     );
 
